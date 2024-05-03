2b2faf3e5736ef3278a7e57cd15d8714e332aa64
Nothingsware <94577071+nothingsware@users.noreply.github.com>
2024-05-03 18:02:56 -0400

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -53,6 +53,121 @@ Tab:AddSlider({
 })
 
 Tab:AddButton({
+	Name = "Fly (X)",
+	Callback = function()
+local plr = game.Players.LocalPlayer
+        local mouse = plr:GetMouse()
+    
+        localplayer = plr
+    
+        if workspace:FindFirstChild("Core") then
+            workspace.Core:Destroy()
+        end
+    
+        local Core = Instance.new("Part")
+        Core.Name = "Core"
+        Core.Size = Vector3.new(0.05, 0.05, 0.05)
+    
+        spawn(function()
+            Core.Parent = workspace
+            local Weld = Instance.new("Weld", Core)
+            Weld.Part0 = Core
+            Weld.Part1 = localplayer.Character.LowerTorso
+            Weld.C0 = CFrame.new(0, 0, 0)
+        end)
+    
+        workspace:WaitForChild("Core")
+    
+        local torso = workspace.Core
+        flying = true
+        local speed=7.5
+        local keys={a=false,d=false,w=false,s=false}
+        local e1
+        local e2
+        local function start()
+            local pos = Instance.new("BodyPosition",torso)
+            local gyro = Instance.new("BodyGyro",torso)
+            pos.Name="EPIXPOS"
+            pos.maxForce = Vector3.new(math.huge, math.huge, math.huge)
+            pos.position = torso.Position
+            gyro.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+            gyro.cframe = torso.CFrame
+            repeat
+                wait()
+                localplayer.Character.Humanoid.PlatformStand=true
+                local new=gyro.cframe - gyro.cframe.p + pos.position
+                if not keys.w and not keys.s and not keys.a and not keys.d then
+                    speed=7.5
+                end
+                if keys.w then
+                    new = new + workspace.CurrentCamera.CoordinateFrame.lookVector * speed
+                    speed=speed+0
+                end
+                if keys.s then
+                    new = new - workspace.CurrentCamera.CoordinateFrame.lookVector * speed
+                    speed=speed+0
+                end
+                if keys.d then
+                    new = new * CFrame.new(speed,0,0)
+                    speed=speed+0
+                end
+                if keys.a then
+                    new = new * CFrame.new(-speed,0,0)
+                    speed=speed+0
+                end
+                if speed>3.2 then
+                    speed=7.5
+                end
+                pos.position=new.p
+                if keys.w then
+                    gyro.cframe = workspace.CurrentCamera.CoordinateFrame*CFrame.Angles(-math.rad(speed*0),0,0)
+                elseif keys.s then
+                    gyro.cframe = workspace.CurrentCamera.CoordinateFrame*CFrame.Angles(math.rad(speed*0),0,0)
+                else
+                    gyro.cframe = workspace.CurrentCamera.CoordinateFrame
+                end
+            until flying == false
+            if gyro then gyro:Destroy() end
+            if pos then pos:Destroy() end
+            flying=false
+            localplayer.Character.Humanoid.PlatformStand=false
+            speed=7.5
+        end
+        e1=mouse.KeyDown:connect(function(key)
+            if not torso or not torso.Parent then flying=false e1:disconnect() e2:disconnect() return end
+            if key=="w" then
+                keys.w=true
+            elseif key=="s" then
+                keys.s=true
+            elseif key=="a" then
+                keys.a=true
+            elseif key=="d" then
+                keys.d=true
+            elseif key=="x" then
+                if flying==true then
+                    flying=false
+                else
+                    flying=true
+                    start()
+                end
+            end
+        end)
+        e2=mouse.KeyUp:connect(function(key)
+            if key=="w" then
+                keys.w=false
+            elseif key=="s" then
+                keys.s=false
+            elseif key=="a" then
+                keys.a=false
+            elseif key=="d" then
+                keys.d=false
+            end
+        end)
+        start() 		
+    end
+})
+
+Tab:AddButton({
 	Name = "Reset JumpPower",
 	Callback = function()
         game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
@@ -76,7 +191,7 @@ Tab:AddSlider({
 Tab:AddButton({
 	Name = "FOV",
 	Callback = function()
-        game.Workspace.CurrentCamera.FieldOfView = 70
+        game.Workspace.CurrentCamera.FieldOfView = 50
   	end    
 })
 
@@ -643,7 +758,7 @@ Tab:AddButton({
   	end    
 })
 
-local espActive = false
+local espActive = true
 
 Tab:AddToggle({
     Name = "Universal ESP",
@@ -692,9 +807,10 @@ Tab:AddToggle({
 
 
 local Credits = Window:MakeTab({
-    Name = "Credits",
+    Name = "Credits & Info",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 
-Credits:AddParagraph("Solara Hub","Solara Hub is made by .bluuu. and brennen_n there discord, they have decided to make this as a script hub to make it easier for people to find scripts that are functional using solara, all scripts made will work and will be checked and updated every day! ")
+Credits:AddParagraph("Solara Hub","Solara Hub is a Script Hub made for Solara, making it easier to find scripts that work on Solara. Every script will be functional and will be checked for updates.")
+Credits:AddParagraph("Founders & Developers","Solara Hub was founded and developed by .bluuu. & brennen_n.")
